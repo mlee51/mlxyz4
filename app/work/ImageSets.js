d29f1039e4b46119028b6015a5eb133a3e17935f
mlee51 <mleeteak13@gmail.com>
2024-03-20 17:19:11 -0400

    root margin error fix

--- app/work/ImageSets.js
+++ app/work/ImageSets.js
@@ -11,7 +11,7 @@ export default function ImageSets({ setTracker, data, projectIndex, onLoad }) {
     const { imageUrls, imageSpan, tags, name } = data;
     const { ref, inView } = useInView({
         threshold: 1,
-        rootMargin: "0 0 -50% 0",
+        rootMargin: "0px 0px -50% 0px",
     });
     const hasWebm = hasWebmUrls(imageUrls);
 
